fb1d7b4bda3189e7d003031233f7ff211c0e2563
Ibrokhimweb <ibrokhimgithub@gmail.com>
2022-11-05 17:19:28 +0500

    Navbar qismi deyarli yakunlandi

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import s from "./Navbar.module.css";
 
 // Import assets
 import logo from "../../assets/logo.png";
 
+// Import React Icons
+import { FaPhone } from "react-icons/fa";
+import { GoThreeBars } from "react-icons/go";
+import { GrFormClose } from "react-icons/gr";
+
 function Navbar(props) {
+
+    const [ nav, setNav ] = useState(true);
+
     return (
         <>
             <nav className={s.navbar}>
                 <Link className={s.brand} to="/">
                     <img className={s.logo} src={logo} alt="logo" />
+                    <p>healthy ration</p>
                 </Link>
+                <section className={s.section}>
+                    <div className={s.main}>
+                        <ul>
+                            <li>
+                                <Link className={s.link} to="/feeding">Програмы питания</Link>
+                            </li>
+                            <li>
+                                <Link className={s.link} to="/lunches">Бизнес-ланчи</Link>
+                            </li>
+                            <li>
+                                <Link className={s.link} to="/gastro/shop">Gastro Shop</Link>
+                            </li>
+                            <li>
+                                <Link className={s.link} to="/about">О нас</Link>
+                            </li>
+                            <li>
+                                <Link className={s.link} to="/blog">Блог</Link>
+                            </li>
+                            <div className={s.icons}>
+                                <div className={s.icon}>
+                                    <FaPhone className={s.icon__item}/>
+                                </div>
+                                <div className={s.icon}>
+                                    {
+                                        nav 
+                                        ? <GoThreeBars className={s.icon__item}/>
+                                        : <GrFormClose className={s.icon__item}/> 
+                                    }
+                                </div>
+                            </div>
+                        </ul>
+                        <div className={s.language}>
+                            <h1>RU</h1>
+                            <h1>UA</h1>
+                            <h1>EN</h1>
+                        </div>
+                    </div>
+                    <div className={s.phone__number}>
+                        <p>+38 (068) 949 - 49 -19</p>
+                    </div>
+                </section>
+
             </nav>
         </>
     );
